4ecf6e648f3ff62d866456195e3bc1f52a2dcf28
Josh <gomez.r.josh@gmail.com>
2025-09-29 12:54:27 -0700

    more work

--- day5/aoc-day5-part1.py
+++ day5/aoc-day5-part1.py
@@ -3,17 +3,21 @@
 def print_correct_pages(pages: list[str], rules: list[str]) -> list[str]:
     # for loop to check each line of the pages string
 
+    # todo: need to compare each line in pages with every line in rules. If it passes ALL rules, then add the middle number of the pages. 
+    sum = 0
+    for line in pages:
+        if all(line.index(rule[0]) < line.index(rule[1]) for rule in rules):
+            sum += line[round(len(line)/2)] 
     
-    return
+    return sum 
 
 with open('example.txt', 'r', encoding='utf-8') as f:
     data = f.read()
     rules, pages = data.split('\n\n')
     rules = rules.split('\n')
-    pages = pages.split('\n')
 
-print(rules)
-print(pages)
+pages_array = [line.split(",") for line in pages.strip().splitlines()]
+print(pages_array)
 
 rules_pairs = []
     
@@ -21,6 +25,7 @@ rules_pairs = []
 for line in rules:
     print(line)
     a, b = line.split('|')
-    rules_pairs.append((a, b))
+    rules_pairs.append((a, b)) # create ordered pairs of the rules
 
-print(rules_pairs)
+print(pages[0])
+# print_correct_pages(pages, rules_pairs)
